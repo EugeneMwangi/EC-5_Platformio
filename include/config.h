08d8dfdb0b60c1b39b045f4eb4e209acdf5e5a5e
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-19 12:05:53 +0300

    Initializes the ds3231 timer

--- include/config.h
+++ include/config.h
@@ -6,9 +6,9 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define RTC_ENABLED             0
+#define RTC_ENABLED             1
 #define EC5_ENABLED             1
-#define SET_RTC_TIME_ENABLED    0
+#define SET_RTC_TIME_ENABLED    1
 
 //Define the input pins of the EC-5 Soil Moisture Sensor
 #define EC5_PWR_PIN      16 //This is GPIO16(D0)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
 #include "ds3231.h"
 #include "ec5.h"
+#include "config.h"
+
+#if RTC_ENABLED
+  DS3231 rtc(&Wire);
+#endif // RTC_ENABLED
 
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(115200);
+  #if RTC_ENABLED
+    Wire.begin(SDA_PIN, SCL_PIN);
+    Wire.setClock(CLK_SPEED);
+    #if SET_RTC_TIME_ENABLED
+    //sec, min, hr, dayOfWeek(1=Sunday, 7=Saturday), dayOfMonth(1-31), month year(0-99)
+      rtc.setDS3231Time(28, 05, 12, 7, 19, 9, 20);
+    #endif // SET_RTC_TIME_ENABLED
+  #endif // RTC_ENABLED
 }
 
 void loop() {
