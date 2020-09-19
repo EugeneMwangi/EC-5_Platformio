b3438de2984ca09fd867be43a6b2203e21f68e86
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-19 12:20:33 +0300

    Defines functions and variables

--- include/thingspeak.h
+++ include/thingspeak.h
@@ -1,6 +1,23 @@
 /*******************************************************************************
- * @file igh_air.h
- * @brief manage air sensor data
- * @auther Alucho C. Ayisi
+ * @file thingpeak.h
+ * @brief sends data to thingspeak
+ * @author Eugene Mwangi
  * Copyright (C), Synnefa Green Ltd. All rights reserved.
  *******************************************************************************/
+
+#ifndef THINGSPEAK_H_
+#define THINGSPEAK_H_
+
+#include <Arduino.h>
+
+String apiKey = "apiKey";       // Enter your Write API key from ThingSpeak
+
+const char* ssid = "ssid";           // Give your wifi network name
+const char* pass  = "password";   // Give your wifi network password
+const char* server = "api.thingspeak.com";  
+
+void thingspeakInit();
+void sendToThingspeak();
+void sendUpdate();
+
+#endif // THINGSPEAK_H_
