ee88218c87c120b3f5bb5662096641be97d147c1
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-19 12:14:00 +0300

    Initial Commit

---
+++ include/thingspeak.h
@@ -0,0 +1,6 @@
+/*******************************************************************************
+ * @file igh_air.h
+ * @brief manage air sensor data
+ * @auther Alucho C. Ayisi
+ * Copyright (C), Synnefa Green Ltd. All rights reserved.
+ *******************************************************************************/
