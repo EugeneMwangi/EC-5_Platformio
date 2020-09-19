817b703cc774babeb9a8b916bec8423b569070e5
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-19 11:56:32 +0300

    Updates config file

---
+++ include/config.h
@@ -0,0 +1,21 @@
+/**
+ * Cofiguration file for the nodeMCU
+ * 
+*/
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define RTC_ENABLED             0
+#define EC5_ENABLED             1
+#define SET_RTC_TIME_ENABLED    0
+
+//Define the input pins of the EC-5 Soil Moisture Sensor
+#define EC5_PWR_PIN      16 //This is GPIO16(D0)
+#define EC5_INPUT        A0   
+
+//Define the SDA & SCL Pins and the CLOCK Speed of the DS3231 
+#define SDA_PIN          4
+#define SCL_PIN          0
+#define CLK_SPEED        400000L // sets I2C clock to 400kHz
+#endif // CONFIG_H_
