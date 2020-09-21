efb2644c7d89d3a2673d9c5efe423a1387e7e239
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-21 11:07:02 +0300

    Defines sendToThingspeak()

--- src/thingspeak.cpp
+++ src/thingspeak.cpp
@@ -7,6 +7,8 @@
 
 
 #include "thingspeak.h"
+#include "ec5.h"
+
 String apiKey = "apiKey";       // Enter your Write API key from ThingSpeak
 
 const char* ssid = "ssid";           // Give your wifi network name
@@ -14,6 +16,10 @@ const char* pass  = "password";   // Give your wifi network password
 const char* server = "api.thingspeak.com";  
 
 WiFiClient client;
+
+/**
+ * @brief Initializes Wifi communication
+*/
 
 void thingspeakInit()
 {
@@ -30,8 +36,36 @@ void thingspeakInit()
     Serial.println("");
     Serial.println("WiFi connected");  
 }
-
+/**
+ * @brief Sends values to ThingSpeak
+*/
 void sendToThingspeak()
 {
-    //
+    float vwcTSVal = ec5VWCReading();
+    float rawAvg = ec5VoltageReading(); //Change this to ec5RawReading
+
+    if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+    {  
+        String postStr = apiKey;
+        postStr +="&field1=";
+        postStr += String(vwcTSVal);
+        postStr +="&field2=";
+        postStr += String(rawAvg);
+        postStr += "\r\n\r\n";
+
+        client.print("POST /update HTTP/1.1\n");
+        client.print("Host: api.thingspeak.com\n");
+        client.print("Connection: close\n");
+        client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+        client.print("Content-Type: application/x-www-form-urlencoded\n");
+        client.print("Content-Length: ");
+        client.print(postStr.length());
+        client.print("\n\n");
+        client.print(postStr);
+        }
+        client.stop();
+
+        Serial.println("Waiting...");     
+        // thingspeak needs minimum 15 sec delay between updates
+        delay(1000);      
 }
